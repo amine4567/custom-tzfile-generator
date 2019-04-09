31fefd8e4da31633507cfb2879737d390dee8f68
asouiki <souikiamine@gmail.com>
2019-04-09 22:43:55 +0200

    initial commit

---
+++ custom_tz.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+def to_tzfile(df: pd.DataFrame):
+    # magic header
+    magic = b"TZif"
+
+    # Decisions
+    format_version= b"2"
+    leapcnt = 0
+
+    transitions = pd.to_datetime(df[["year", "month", "day", "hour", "minute", "second"]].drop(0))
+    #TODO: Convert pandas timestamps to datetime.datetime. Then convert transitions Series to list 
+    #with tolist() method
+
+    print(transitions)
+if __name__=="__main__":
+    df_paris = pd.read_csv("paris_custom_format.csv")
+    to_tzfile(df_paris)
+    import pytz
+    #print(df_paris)
