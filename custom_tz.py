5f46e47137da1f62f252596d2c26df8310cfb8fd
asouiki <souikiamine@gmail.com>
2019-04-09 22:51:48 +0200

    transitions now contains a list of datetime.datetime objects. Added black for auto-formatting

--- custom_tz.py
+++ custom_tz.py
@@ -1,20 +1,24 @@
 import pandas as pd
 
+
 def to_tzfile(df: pd.DataFrame):
     # magic header
     magic = b"TZif"
 
     # Decisions
-    format_version= b"2"
+    format_version = b"2"
     leapcnt = 0
 
-    transitions = pd.to_datetime(df[["year", "month", "day", "hour", "minute", "second"]].drop(0))
-    #TODO: Convert pandas timestamps to datetime.datetime. Then convert transitions Series to list 
-    #with tolist() method
+    transitions = pd.to_datetime(
+        df[["year", "month", "day", "hour", "minute", "second"]].drop(0)
+    ).tolist()
+    transitions = [elt.to_pydatetime() for elt in transitions]
+    # TODO: Convert pandas timestamps to datetime.datetime. Then convert transitions Series to list
+    # with tolist() method
 
     print(transitions)
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     df_paris = pd.read_csv("paris_custom_format.csv")
     to_tzfile(df_paris)
-    import pytz
-    #print(df_paris)
